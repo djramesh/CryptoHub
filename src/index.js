40c10f52db03c5bf12a41dad6ad5fe3852f552e5
Deep Baishya <deepbaishya4385@gmail.com>
2023-09-25 20:03:00 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   </React.StrictMode>
 );
 
-export const server='http://api.coingecko.com/api/v3'
+export const server='https://api.coingecko.com/api/v3'
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
